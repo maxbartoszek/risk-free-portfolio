0125ddde3a9416341723f12e2675dee17351aa33
wendixue17 <wendixue17@gmail.com>
2025-11-08 11:51:51 -0500

    added code for filtering stock list (filter for volumes not done yet)

--- base code.py
+++ base code.py
@@ -13,19 +13,51 @@ import random
 from datetime import datetime
 
 # Load the tickers
-df = pd.read_csv('Tickers_Example.csv') # test with Tickers_Example.csv?
+df = pd.read_csv('Tickers_Example.csv', header=None) # test with Tickers_Example.csv?
 ticker_lst = list(df.iloc[:,0])
 
 # Filter out tickers that don't exist; include only valid tickers
+valid_tickers_lst = []
+for i in range(len(ticker_lst)):
+    ticker_data = yf.Ticker(ticker_lst[i]) # call up data
+    country = ticker_data.info.get('country') 
+    currency = ticker_data.info.get('currency')
+    if country in {'Canada','United States'} and currency in {'CAD','USD'}: # filters us and canadian tickers and listed stocks
+        valid_tickers_lst.append(ticker_lst[i])
 
-# Apply a filter to remove all stocks with volumes below 5000
+print(valid_tickers_lst)
+
+# Apply a filter to remove all stocks with volumes below 5000 (in the given period)
+# period in which we look at the volumes 
+volume_start_date = '2024-10-01'
+volume_end_date = '2025-09-30' 
+for i in range(len(valid_tickers_lst)): # goes through every ticker to filter
+    filtered_lst = []
+    data = yf.download(
+        tickers=valid_tickers_lst[i],
+        start=volume_start_date,
+        end=volume_end_date
+    )
+    volume_data = data['Volume'].dropna() # volume data
+
+    dropped_months = [] # df of all the months with more than 18 trading days
+    volume_data['Month'] = volume_data.to_period('M') # create new column of index only by (YYYY-MM)
+    grouped_month_index = volume_data.groupby(['Month']) # group data by month
+    for month, group in grouped_month_index:
+        if group.size > 18: # if the month has more than 18 trading days
+            dropped_months.append(month) # add data of months with more than 18 trading days
+    average_daily_volume = dropped_months['Volume'].mean() # calculate average daily volume
+    if average_daily_volume >= 5000: # determine if above or below 5000 shares
+        filtered_lst.append(valid_tickers_lst[i]) # add to filtered list if greater or equal to 5000 shares
+
+print(filtered_lst)
 
 # Get the daily data over chosen timeframe (2025-10-24 to 2025-10-31 for testing?)
 start_date = '2025-10-24' # change to Nov 21 2025
 end_date = '2025-10-31' # change to Nov 28 2025
 
 daily_data = yf.download(
-    tickers=ticker_lst,
+    tickers=filtered_lst,
     start=start_date,
     end=end_date)
 
